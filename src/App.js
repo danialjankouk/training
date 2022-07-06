4db24549579bc320f505c14d1e06c8a3822bb5ae
danialjankouk <dani.jankook@gmail.com>
2022-07-06 20:02:06 +0430

    darkmode - system base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+//components
+import Main from "./components/Main";
 const App = () => {
   return (
-    <div>
-
+    <div className="dark:bg-slate-800 h-screen bg-slate-200 container mx-auto flex justify-center items-center">
+      <Main/>
     </div>
   );
 };
