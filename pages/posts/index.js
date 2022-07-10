583a2d07eafae0b04103ec262a19878e8b1c0682
danialjankouk <dani.jankook@gmail.com>
2022-07-10 20:30:34 +0430

    routing

---
+++ pages/posts/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            this is home page in posts
+        </div>
+    );
+};
+
+export default Home;
---
+++ pages/posts/p1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const p1 = () => {
+    return (
+        <div>
+            this is page1 in post
+        </div>
+    );
+};
+
+export default p1;
