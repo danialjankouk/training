f6d808658374246d33c9a202959fca87af4e8e95
danialjankouk <dani.jankook@gmail.com>
2022-08-02 15:42:39 +0430

    todoApp with authentication

---
+++ src/pages/protected-ssr.js
@@ -0,0 +1,31 @@
+import Layout from "@/containers/Layout";
+import { getSession, useSession } from "next-auth/react";
+
+const ProtectedSSR = () => {
+  const { data: session, status } = useSession();
+
+  return (
+    <Layout> 
+      <h1>{session.user.name},wellcome to protected SSR Page</h1>
+    </Layout>
+  );
+};
+
+export default ProtectedSSR;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/signin?callbackUrl=http://localhost:3000/protected-ssr",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
+}
