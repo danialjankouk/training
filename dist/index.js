085a080f55267094aa840992d854dccc7aa3c31f
Danial <85788131+danialjankouk@users.noreply.github.com>
2022-10-23 18:08:50 +0330

    any type

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,9 @@
 "use strict";
-const age = 30;
-console.log("hello word");
+let age = 3045;
+let course = "this is shit";
+let is_public = true;
+let vel;
+const render = (fun, dick) => {
+    console.log(fun, dick);
+};
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-const age: number = 30;
-console.log("hello word");  
+let age: number = 30_45;
+let course: string = "this is shit";
+let is_public: boolean = true; 
+let vel;
+
+const render = (fun: any,test: string) => {
+    console.log(fun,test);
+}
