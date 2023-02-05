48e6a1d02b5d603c17b766617c90cc786c9b25d2
Danial <85788131+danialjankouk@users.noreply.github.com>
2023-02-05 17:55:30 +0330

    types

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-var test = "hello world";
-console.log(test);
+//number
+var Likes = 55;
+console.log(typeof Likes);
+Likes = 5;
+//string
+var Note = "typescript is cool";
+console.log(typeof Note);
+// boolean
+var Bool = false;
+console.log(typeof Bool);
+Bool = true;
+//share type
+var user;
+user = "dany j";
+//backtick
+var str = "this is ".concat(Bool);
+console.log(str);
+//
--- index.ts
+++ index.ts
@@ -1,2 +1,23 @@
-const test = "hello world";
-console.log(test);
+//number
+let Likes: number = 55;
+console.log(typeof Likes);
+Likes = 5;
+//string
+let Note: string = "typescript is cool";
+console.log(typeof Note);
+// boolean
+let Bool: boolean = false;
+console.log(typeof Bool);
+Bool = true;
+
+//share type
+let user: typeof Note;
+user = "dany j";
+
+//backtick
+let str: string = `this is ${Bool}`;
+console.log(str);
+
+//mix
+console.log(Likes * 85);
+
