a2488c33d7743dbd6b15a262c3550627eca4f599
Danial <85788131+danialjankouk@users.noreply.github.com>
2023-02-18 12:38:29 +0330

    types

---
+++ types.js
@@ -0,0 +1,8 @@
+// null
+var nulf = null;
+//undefind
+var und = undefined;
+//void
+var voi = function () {
+    return;
+};
---
+++ types.ts
@@ -0,0 +1,14 @@
+// null
+let nulf: null = null;
+//undefind
+let und: undefined = undefined;
+//void
+function check():void{
+    
+}
+//any
+const an: any = undefined
+//unknown => for get data from backend
+const unk:unknown = 5
+//never => return error
+const nev = 5
