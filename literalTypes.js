0ebf5d77d884576ee2c8d47affcb805ffcdfd95c
Danial <85788131+danialjankouk@users.noreply.github.com>
2023-03-11 21:11:52 +0330

    more literal type

--- literalTypes.js
+++ literalTypes.js
@@ -1,1 +1,11 @@
 var developPacckage = "node js";
+var newT;
+(function (newT) {
+    newT[newT["pending"] = 0] = "pending";
+    newT[newT["starter"] = 1] = "starter";
+    newT[newT["action"] = 2] = "action";
+})(newT || (newT = {}));
+var fdunc = {
+    types: "typescript",
+    model: newT.pending
+};
--- literalTypes.ts
+++ literalTypes.ts
@@ -2,3 +2,14 @@
 type packagee = "typescript" | "react" | "node js"
 
 let developPacckage:packagee = "node js"  
+
+enum newT {
+    pending ,
+    starter,
+    action
+}
+
+let fdunc:{types:packagee , model:newT} = {
+    types:"typescript",
+    model:newT.pending
+}
